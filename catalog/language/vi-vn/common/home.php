9a5f1f83572e0672225684dbc1fa284bdc5e02ce
Minh Tuan <minhtuan9922@gmail.com>
2020-04-04 00:25:26 +0700

    cap nhat

--- catalog/language/vi-vn/common/home.php
+++ catalog/language/vi-vn/common/home.php
@@ -1,6 +1,7 @@
 <?php
 // Text
 $_['text_title_1']    = 'Quy tình tư vấn du học';
+$_['text_title_2']    = 'THÀNH TỰU & GIẢI THƯỞNG';
 $_['text_content_1']  = 'Là thương hiệu lớn trong lĩnh vực du học và mang trên mình sứ mệnh thắp sáng tương lai cho thế hệ trẻ Việt Nam, Trí nhất luôn đặt chữ TÍN lên hàng đầu và áp dụng những tiêu chuẩn quốc tế vào quy trình làm việc.';
 $_['text_step_1']  = 'Tư vấn';
 $_['text_step_2']  = 'Lên kế hoạch';
